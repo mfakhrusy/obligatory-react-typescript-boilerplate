14f160ea78fe00cd4cb1ed3eea62bc50336c3cc8
Muhamad Fakhrusy <mfakhrusy@outlook.com>
2019-06-06 11:02:17 +0700

    add simple react typescript boilerplate

---
+++ src/containers/App/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+import "./index.scss";
+
+export class App extends React.Component<{}, {}> {
+  public render() {
+    return (
+      <div>
+        hello world
+      </div>
+    );
+  }
+}
